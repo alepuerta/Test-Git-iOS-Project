3a7cfe9b54efcc06598f36409572d8c3460885d7
Alejandro <alejandro@Alejandros-Mac.local>
2014-10-30 23:35:10 +0000

    Initial Commit

---
+++ Git Test/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  Git Test
+//
+//  Created by Alejandro on 30/10/14.
+//  Copyright (c) 2014 Alejandro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
