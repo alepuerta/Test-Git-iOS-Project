f33ef2b1857c03ce00268724cef14379a53ff998
Alejandro <alejandro@Alejandros-Mac.local>
2014-10-30 23:51:40 +0000

    added a test class

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Alejandro on 30/10/14.
+//  Copyright (c) 2014 Alejandro. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Alejandro on 30/10/14.
+//  Copyright (c) 2014 Alejandro. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
